b7f044bbf0ccf90834b79022a74014fa479da339
kirin <kirin.chen1001@gmail.com>
2021-04-12 22:26:59 +0800

    fix post perciem issue

--- rest/post_order.py
+++ rest/post_order.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+import uuid
 from binance_f import RequestClient
 from binance_f.model import OrderSide, OrderType, TimeInForce, WorkingType, PositionSide, AccountInformation
 from rest.poxy_controller import PayloadReqKey
@@ -9,6 +9,7 @@ class PayloadKey(Enum):
     investedRate = 'investedRate'
     guardRange = 'guardRange'
     quote = 'quote'
+    symbol = 'symbol'
 
 
 def _get_val(payload: dict, k: PayloadKey):
@@ -16,7 +17,8 @@ def _get_val(payload: dict, k: PayloadKey):
 
 
 def fix_precision(fv: float):
-    return float('{:.2f}'.format(fv))
+    ans = float('{:.2f}'.format(fv))
+    return str(ans)
 
 
 def run(client: RequestClient, payload: dict):
@@ -28,21 +30,47 @@ def run(client: RequestClient, payload: dict):
     quantity = (amount * leverage_ratio) / quote
     max_stop = quote * (1 + _get_val(payload, PayloadKey.guardRange))
 
-    od = {
-        TODO ID
-        "price": fix_precision(quote),
-        "side": OrderSide.SELL,
-        "symbol": "ETCUSDT",
-        "timeInForce": TimeInForce.GTC,
-        "ordertype": OrderType.LIMIT,
-        "workingType": WorkingType.CONTRACT_PRICE,
-        "positionSide": PositionSide.SHORT,
-        "activationPrice": None,
-        "closePosition": False,
-        "quantity": fix_precision(quantity)
-    }
-    TODO stop Profit loss
-    result = client.post_order(**od)
+    # od = {
+    #     TODO ID
+    #     "price": fix_precision(quote),
+    #     "side": OrderSide.SELL,
+    #     "symbol": "ETCUSDT",
+    #     "timeInForce": TimeInForce.GTC,
+    #     "ordertype": OrderType.LIMIT,
+    #     "workingType": WorkingType.CONTRACT_PRICE,
+    #     "positionSide": PositionSide.SHORT,
+    #     "activationPrice": None,
+    #     "closePosition": False,
+    #     "quantity": fix_precision(quantity)
+    # }
+
+    oid = str(uuid.uuid4())
+
+    result = client.post_order(price=fix_precision(quote),
+                               side=OrderSide.SELL,
+                               symbol=_get_val(payload, PayloadKey.symbol),
+                               timeInForce=TimeInForce.GTC,
+                               ordertype=OrderType.LIMIT,
+                               workingType=WorkingType.CONTRACT_PRICE,
+                               positionSide=PositionSide.SHORT,
+                               activationPrice=None,
+                               closePosition=False,
+                               quantity=fix_precision(quantity),
+                               newClientOrderId=oid
+                               )
+
+    result = client.post_order(
+                               side=OrderSide.SELL,
+                               symbol=_get_val(payload, PayloadKey.symbol),
+                               timeInForce=TimeInForce.GTC,
+                               ordertype=OrderType.TAKE_PROFIT_MARKET ,
+                               workingType=WorkingType.CONTRACT_PRICE,
+                               positionSide=PositionSide.SHORT,
+                               stopPrice=fix_precision(max_stop),
+                               closePosition=False,
+                               quantity=fix_precision(quantity)
+                               )
+
     return {}
 
 
