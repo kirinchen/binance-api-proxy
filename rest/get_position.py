ca1a064f6b70b3bc2ac5fc7e9cdb944e6ccd3d77
kirin <kirin.chen1001@gmail.com>
2021-05-10 19:58:46 +0800

    get postiop by filter

--- rest/get_position.py
+++ rest/get_position.py
@@ -6,11 +6,14 @@ from binance_f.constant.test import *
 from binance_f.base.printobject import *
 from binance_f.model import Position
 from binance_f.model.constant import *
+from rest.poxy_controller import PayloadReqKey
+from utils.position_utils import PositionFilter, filter_position
 
 
 def run(client: RequestClient, payload: dict):
+    PayloadReqKey.clean_default_keys(payload)
+    pf = PositionFilter(**payload)
     result: List[Position] = client.get_position()
-    pos= [r.__dict__ for r in result]
+    result = filter_position(result, pf)
+    pos = [r.__dict__ for r in result]
     return pos
-
-
---
+++ utils/position_utils.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from binance_f.model import Position
+from market.Symbol import Symbol
+
+
+class PositionFilter:
+
+    def __init__(self, symbol: str = None, positionSide: str = None):
+        self.symbol = symbol
+        self.positionSide = positionSide
+
+    def get_symbole(self) -> Symbol:
+        return Symbol.get(self.symbol)
+
+
+def filter_position(ps: List[Position], ft: PositionFilter) -> List[Position]:
+    ans: List[Position] = list()
+    for p in ps:
+
+        if ft.symbol and ft.get_symbole().gen_with_usdt() != p.symbol:
+            continue
+        if ft.positionSide and ft.positionSide != p.positionSide:
+            continue
+        ans.append(p)
+    return ans
