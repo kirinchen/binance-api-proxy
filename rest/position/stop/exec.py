5b46560f00b7cee3ef1a82f8a69d5b4bb0cf886e
kirin <kirin.chen1001@gmail.com>
2021-09-29 13:45:52 +0800

    add try catch

--- rest/position/stop/exec.py
+++ rest/position/stop/exec.py
@@ -5,8 +5,13 @@ from utils import comm_utils
 
 
 def run(client: RequestClient, payload: dict):
-    PayloadReqKey.clean_default_keys(payload)
-    dto = StopMediationDto(**payload)
-    stopMediation = StopMediation(client=client, dto=dto)
+    try:
+        PayloadReqKey.clean_default_keys(payload)
+        dto = StopMediationDto(**payload)
+        stopMediation = StopMediation(client=client, dto=dto)
 
-    return comm_utils.to_dict(stopMediation.stop())
+        return comm_utils.to_dict(stopMediation.stop())
+    except Exception as e:  # work on python 3.x
+        return str(e)
+
+
